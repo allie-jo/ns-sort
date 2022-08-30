b3de34ec592b1e3c57b2c6aff46c18f03882bc69
Allison Casey <allison.casey@reifyhealth.com>
2022-08-30 09:11:45 -0700

    refactor: rewrite-clj complete rewrite

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject ns-sort "1.0.0"
   :description "Sort :require block in Clojure namespaces"
   :url "http://github.com/ilevd/ns-sort"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [rewrite-clj/rewrite-clj "1.1.45"]]
   :profiles {:uberjar
              {:aot [ns-sort.core],
               :main ns-sort.core
@@ -9,9 +10,8 @@
               :uberjar-name "ns-sort-%s"}}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :jvm-opts
-  ^:replace ["-server"
-             "-Xms2048m"
-             "-Xmx2048m"
-             "-Xss500m"
-             "-XX:-OmitStackTraceInFastThrow"])
+  :jvm-opts ^:replace ["-server"
+                       "-Xms2048m"
+                       "-Xmx2048m"
+                       "-Xss500m"
+                       "-XX:-OmitStackTraceInFastThrow"])
