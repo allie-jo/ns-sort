4ac877ad13b10fb7f9133db23604a0027f4c6e65
Allison Casey <allison.casey@reifyhealth.com>
2022-10-07 16:00:57 -0700

    test: add test for various ns form permutations

--- test/ns_sort_test.clj
+++ test/ns_sort_test.clj
@@ -48,3 +48,11 @@
 (deftest update-code-test
   (is (= (ns-sort/update-code (slurp "test/data/unsorted.clj-test"))
          (slurp "test/data/sorted.clj-test"))))
+
+(deftest ns-permutations-test
+  (let [unchanged? (fn [s] (= s (ns-sort/update-code s)))]
+    (is (unchanged? "(ns test-ns)"))
+    (is (unchanged? "(ns test-ns \"docstring\")"))
+    (is (unchanged? "(ns test-ns (:require [clojure.string :as str]))"))
+    (is (unchanged? "(ns test-ns (:import (java.io File)))"))
+    (is (unchanged? "(ns test-ns (:require [clojure.string :as str]) (:import (java.io File)))"))))
